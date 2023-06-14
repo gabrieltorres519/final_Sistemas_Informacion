ac9e20812794846de728d576afd30707684bf4d9
unknown <gabrieltorres9909@gmail.com>
2023-06-13 20:52:48 -0600

    agregando la uri mongo

--- src/database.js
+++ src/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/final_sistemas')
+// mongoose.connect('mongodb://localhost/final_sistemas')
+mongoose.connect('mongodb+srv://gatorresmendoza:7tphDXuZhi6D1BGy@cluster0.7g1ama0.mongodb.net/?retryWrites=true&w=majority')
     .then(db => console.log('BASE DE DATOS CONECTADA', db.connection.host))
     .catch(err => console.error(err));
